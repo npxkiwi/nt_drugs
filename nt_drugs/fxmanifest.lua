bf3ee00470646ba9938f7a103ddb5c357ca3b652
V4cuum <151445483+npxkiwi@users.noreply.github.com>
2024-03-03 12:19:31 +0100

    Update fxmanifest.lua

--- nt_drugs/fxmanifest.lua
+++ nt_drugs/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'Notepad x Kiwi x Dallefar'
-description 'A drug script by Notepad and Kiwi, Dallefar'
+description 'A drug script by Notepad, Kiwi and Dallefar'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -18,5 +18,6 @@ client_scripts {
 
 server_scripts {
     '@vrp/lib/utils.lua',
+    'Config.lua',
     'server.lua'
 }
